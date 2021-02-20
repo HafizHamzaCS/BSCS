9939c2cf9bbf32e6610813ccbd9f8365b9493618
HafizHamzaCS <53398950+HafizHamzaCS@users.noreply.github.com>
2021-02-20 14:39:37 +0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+namespace racing_car_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            int speed=""; 
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            speed = 3;
+
+        }
+
+        private void RoadMover(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
